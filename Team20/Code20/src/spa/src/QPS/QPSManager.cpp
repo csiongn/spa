fc843320b117e1f18dec44e2efb9f2674a3d0d2c
Cai Zhibin <68769529+czhi-bin@users.noreply.github.com>
2024-02-26 15:29:25 +0800

    chore(#212, #214): add handling of exception for QPS (#215)

--- Team20/Code20/src/spa/src/QPS/QPSManager.cpp
+++ Team20/Code20/src/spa/src/QPS/QPSManager.cpp
@@ -1,5 +1,7 @@
 #include "QPSManager.h"
 #include "QueryParser.h"
+#include "QuerySemanticError.h"
+#include "QuerySyntaxError.h"
 
 QPSManager::QPSManager(std::shared_ptr<IPKBReader> pkb) : tokenizer(), evaluator(pkb) {};
 
@@ -7,11 +9,18 @@ void QPSManager::evaluate(const std::string &query, std::list<std::string> &resu
     std::vector<std::shared_ptr<QueryToken>> tokens = tokenizer.tokenize(query);
     QueryParser parser(tokens);
 
-    auto q = parser.parse();
-
-    std::vector<std::string> res = evaluator.evaluateQuery(q);
-    for (const auto &string : res) {
-        results.push_back(string);
+    try {
+        auto q = parser.parse();
+        std::vector<std::string> res = evaluator.evaluateQuery(q);
+        for (const auto &string : res) {
+            results.push_back(string);
+        }
+    } catch (const QuerySyntaxError &e){
+        results.emplace_back(e.what());
+    } catch (const QuerySemanticError &e) {
+        results.emplace_back(e.what());
+    } catch (...) {
+        // unknown error, does nothing
     }
 }
 
