6d0074d71b1759655092695b6877d927a6031156
Choon Siong <37247001+csiongn@users.noreply.github.com>
2024-03-16 22:25:02 +0800

    test(#359): add mock pkb to design extractor unit tests (#360)

--- Team20/Code20/src/unit_testing/src/SP/TestDesignExtractor.cpp
+++ Team20/Code20/src/unit_testing/src/SP/TestDesignExtractor.cpp
@@ -1,7 +1,247 @@
+#include <set>
+
 #include "catch.hpp"
 #include "PKB/PKB.h"
 #include "SP/AST.h"
 #include "SP/DesignExtractor.h"
+
+class TestException final : public std::exception {
+    // https://www.geeksforgeeks.org/how-to-throw-custom-exception-in-cpp/
+    // https://stackoverflow.com/questions/41753358/creating-custom-exceptions-in-c
+    std::string message;
+public:
+    explicit TestException(const std::string& msg) : message(msg) {}
+
+    const char* what() const noexcept override {
+        return message.c_str();
+    }
+};
+
+class MockPKBWriter : public IPKBWriter {
+public:
+    // Stored entities and relations
+    std::unordered_set<std::string> insertedProcedures;
+    std::unordered_set<std::string> insertedVariables;
+    std::unordered_set<int> insertedConstants;
+    std::unordered_set<int> insertedStatements;
+    std::unordered_set<int> insertedAssigns;
+    std::unordered_set<int> insertedReads;
+    std::unordered_set<int> insertedPrints;
+    std::unordered_set<int> insertedCalls;
+    std::unordered_set<int> insertedWhiles;
+    std::unordered_set<int> insertedIfs;
+    std::vector<std::pair<int, int>> followsRelations;
+    std::vector<std::pair<int, int>> followsTRelations;
+    std::vector<std::pair<int, int>> parentRelations;
+    std::vector<std::pair<int, int>> parentTRelations;
+    std::vector<std::pair<int, const std::string>> usesRelations;
+    std::vector<std::pair<std::string, std::string>> procUsesRelations;
+    std::vector<std::pair<int, const std::string>> modifiesRelations;
+    std::vector<std::pair<std::string, std::string>> procModifiesRelations;
+    std::vector<std::pair<std::string, std::string>> callRelations;
+    std::vector<std::pair<std::string, std::string>> callTRelations;
+    std::vector<std::tuple<std::string, size_t, int, std::shared_ptr<ExprNode>>> assignPatterns;
+    std::vector<std::tuple<std::string, size_t, int, std::shared_ptr<ExprNode>>> assignPartialPatterns;
+    std::vector<std::pair<const std::string, const int>> ifPatterns;
+    std::vector<std::pair<const std::string, const int>> whilePatterns;
+
+
+    void insertProcedure(std::string const & procedureName) override {
+        insertedProcedures.insert(procedureName);
+    }
+
+    void insertProcedure(const std::unordered_set<std::string>& procedureSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertVariable(std::string const & variableName) override {
+        insertedVariables.insert(variableName);
+    }
+
+    void insertVariable(const std::unordered_set<std::string>& variableSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertConstant(int constant) override {
+        insertedConstants.insert(constant);
+    }
+
+    void insertConstant(const std::unordered_set<int>& constantSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertStatement(int stmtNum) override {
+        insertedStatements.insert(stmtNum);
+    }
+
+    void insertStatement(const std::unordered_set<int>& stmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertAssign(int stmtNum) override {
+        insertedAssigns.insert(stmtNum);
+    }
+
+    void insertAssign(const std::unordered_set<int>& stmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertRead(int stmtNum) override {
+        insertedReads.insert(stmtNum);
+    }
+
+    void insertRead(const std::unordered_set<int>& stmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertPrint(int stmtNum) override {
+        insertedPrints.insert(stmtNum);
+    }
+
+    void insertPrint(const std::unordered_set<int>& stmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertCall(int stmtNum) override {
+        insertedCalls.insert(stmtNum);
+    }
+
+    void insertCall(const std::unordered_set<int>& stmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertWhile(int stmtNum) override {
+        insertedWhiles.insert(stmtNum);
+    }
+
+    void insertWhile(const std::unordered_set<int>& stmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertIf(int stmtNum) override {
+        insertedIfs.insert(stmtNum);
+    }
+
+    void insertIf(const std::unordered_set<int>& stmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertFollows(int followeeStmtNum, int followerStmtNum) override {
+        followsRelations.emplace_back(followeeStmtNum, followerStmtNum);
+    }
+
+    void insertFollows(int followeeStmtNum, const std::unordered_set<int>& followerStmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertFollowsT(int followeeStmtNum, int followerStmtNum) override {
+        followsTRelations.emplace_back(followeeStmtNum, followerStmtNum);
+    }
+
+    void insertFollowsT(int followeeStmtNum, const std::unordered_set<int>& followerStmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertParent(int parentStmtNum, int childStmtNum) override {
+        parentRelations.emplace_back(parentStmtNum, childStmtNum);
+    }
+
+    void insertParent(int parentStmtNum, const std::unordered_set<int>& childStmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertParentT(int parentStmtNum, int childStmtNum) override {
+        parentTRelations.emplace_back(parentStmtNum, childStmtNum);
+    }
+
+    void insertParentT(int parentStmtNum, const std::unordered_set<int>& childStmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertUsesStmt(int stmtNum, const std::string& variable) override {
+        usesRelations.emplace_back(stmtNum, variable);
+    }
+
+    void insertUsesStmt(int stmtNum, std::unordered_set<std::string> const & variableSet) override {
+        for (auto variable: variableSet) {
+            usesRelations.emplace_back(stmtNum, variable);
+        }
+    }
+
+    void insertUsesProc(std::string const & procedureName, std::string const & variable) override {
+        procUsesRelations.emplace_back(procedureName, variable);
+    }
+
+    void insertUsesProc(std::string const & procedureName, std::unordered_set<std::string> const & variableSet) override {
+        for (auto variable: variableSet) {
+            procUsesRelations.emplace_back(procedureName, variable);
+        }
+    }
+
+    void insertModifiesStmt(int stmtNum, std::string const & variable) override {
+        modifiesRelations.emplace_back(stmtNum, variable);
+    }
+
+    void insertModifiesStmt(int stmtNum, std::unordered_set<std::string> const & variableSet) override {
+        for (auto variable: variableSet) {
+            modifiesRelations.emplace_back(stmtNum, variable);
+        }
+    }
+
+    void insertModifiesProc(std::string const & procedureName, std::string const & variable) override {
+        procModifiesRelations.emplace_back(procedureName, variable);
+    }
+
+    void insertModifiesProc(std::string const & procedureName, std::unordered_set<std::string> const & variableSet) override {
+        for (auto variable: variableSet) {
+            procModifiesRelations.emplace_back(procedureName, variable);
+        }
+    }
+
+    void insertCallsProc(std::string const &caller, std::string const &callee) override {
+        callRelations.emplace_back(caller, callee);
+    }
+
+    void insertCallsProc(std::string const &caller, std::unordered_set<std::string> const &calleeSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertCallsTProc(std::string const &caller, std::string const &callee) override {
+        callTRelations.emplace_back(caller, callee);
+    }
+
+    void insertCallsTProc(std::string const &caller, std::unordered_set<std::string> const &calleeSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertIfPattern(std::string const &variable, const int &stmtNum) override {
+        ifPatterns.emplace_back(variable, stmtNum);
+    }
+
+    void insertIfPattern(std::string const &variable, const std::unordered_set<int> &stmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertWhilePattern(std::string const &variable, const int &stmtNum) override {
+        whilePatterns.emplace_back(variable, stmtNum);
+    }
+
+    void insertWhilePattern(std::string const &variable, const std::unordered_set<int> &stmtSet) override {
+        throw TestException("Method not supposed to be called");
+    }
+
+    void insertAssignPattern(std::string const & lhsVar,
+                             const size_t& rhsExprNodeHash,
+                             const int& stmtNum,
+                             const std::shared_ptr<ExprNode>& nodePtr) override {
+        assignPatterns.emplace_back(lhsVar, rhsExprNodeHash, stmtNum, nodePtr);
+    }
+
+    void insertAssignPartialPattern(std::string const &lhsVar, const size_t &rhsExprNodeHash, const int &stmtNum, const std::shared_ptr<ExprNode> &nodePtr) override {
+        assignPartialPatterns.emplace_back(lhsVar, rhsExprNodeHash, stmtNum, nodePtr);
+    }
+
+};
 
 TEST_CASE("Simple program extractDesign") {
 
@@ -58,9 +298,8 @@ TEST_CASE("Simple program extractDesign") {
     auto program = std::make_shared<ProgramNode>(std::vector{procedure});
 
     // Initialize design extractor
-    auto pkb = std::make_shared<PKB>();
-    std::shared_ptr<PKBFacade> pkbFacade = pkb->pkbFacade;
-    auto extractor = DesignExtractor(pkbFacade);
+    auto pkbWriter = std::make_shared<MockPKBWriter>();
+    auto extractor = DesignExtractor(pkbWriter);
     extractor.extractDesign(*program);
 
     SECTION("Check correct follows relationships") {
